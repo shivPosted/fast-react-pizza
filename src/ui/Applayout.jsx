1ab6223ecffa7316a78cb09047fc53ccaedc0886
shivPosted <shivpratap621@gmail.com>
2024-10-26 01:54:25 +0530

    using useNavigation() hook of router to display a loader when state is 'loading'

--- src/ui/Applayout.jsx
+++ src/ui/Applayout.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Cartoverview from "../features/cart/Cartoverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 function Applayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading"; // checking if the data is loading
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+      {/*displaying loader when data is loading by checking the isLoading derived earlier*/}
       <Header />
       <Outlet />
       <Cartoverview />
